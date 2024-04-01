9efec039e7f06eae5129e7c4263192eaafa8a04d
ProgrammerPeter2 - Ubuntu <hp.raszpi@gmail.com>
2024-04-01 12:19:12 +0200

    set up the basic pipeline for linux

--- scout/Scout/Core.h
+++ scout/Scout/Core.h
@@ -6,4 +6,10 @@
     #else
         #define SCOUT_API __declspec(dllimport)
     #endif
+#elifdef SC_PLATFORM_LINUX
+    #ifdef SC_BUILD_DLL
+        #define SCOUT_API __attribute__((dllexport))
+    #else
+        #define SCOUT_API __attribute__((dllimport))
+    #endif
 #endif
